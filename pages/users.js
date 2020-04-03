4408d927c68f874a482a88e606fc025898dab5bf
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-03 13:18:21 -0400

    link e header

---
+++ pages/users.js
@@ -0,0 +1,34 @@
+import React from "react";
+import axios from "axios";
+import Head from "next/head";
+
+import { Link } from "../routes";
+
+const Users = ({ users }) => (
+  <div>
+    <Head>
+      <title>Usuários</title>
+    </Head>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <Link route={`users/${user.login}`}>
+            <a>{user.login}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+    <Link href="/">
+      <a>Voltar</a>
+    </Link>
+  </div>
+);
+
+Users.getInitialProps = async () => {
+  const response = await axios.get(
+    "https://api.github.com/orgs/dom-pixel/members"
+  );
+  return { users: response.data };
+};
+
+export default Users;
