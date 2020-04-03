beb664024ac907fd4cde65367e34dfa222fbc0a0
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-03 13:24:56 -0400

    _document

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 import React from "react";
+import Link from "next/link";
+import Head from "next/head";
 
-const Home = () => <h1>Hello World</h1>;
+const Home = () => (
+  <div>
+    <Head>
+      <title>Meu App NextJS</title>
+    </Head>
+    <img width="200" src="/static/panda.jpg" />
+    <Link prefetch href="/users">
+      <a>Usuários</a>
+    </Link>
+  </div>
+);
 
 export default Home;
